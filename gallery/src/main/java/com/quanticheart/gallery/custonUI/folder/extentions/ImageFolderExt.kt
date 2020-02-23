0ae8dc34a5e207765664fede4dc66e5cfb756a70
John <jonny255d@gmail.com>
2020-02-23 12:10:32 -0300

    refactor FolderExt

--- gallery/src/main/java/com/quanticheart/gallery/custonUI/folder/extentions/ImageFolderExt.kt
+++ gallery/src/main/java/com/quanticheart/gallery/custonUI/folder/extentions/ImageFolderExt.kt
@@ -39,39 +39,30 @@ package com.quanticheart.gallery.custonUI.folder.extentions
 
 import android.content.Context
 import android.provider.MediaStore
-import android.util.Log
 import com.quanticheart.gallery.custonUI.folder.model.ImageFolderData
-import java.util.*
+import com.quanticheart.gallery.extentions.getStringOrEmpty
 
 fun Context.getAllImageFolders(): ArrayList<ImageFolderData> {
 
     val picFolderData: ArrayList<ImageFolderData> = ArrayList()
     val picPaths = ArrayList<String>()
 
-    val allImagesuri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+    val allImagesUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
 
-    val projection = arrayOf(
-        MediaStore.Images.ImageColumns.DATA,
-        MediaStore.Images.Media.DISPLAY_NAME,
-        MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
-        MediaStore.Images.Media.BUCKET_ID
-    )
+    val projection = getDatabaseKeys()
 
-    val cursor = contentResolver.query(allImagesuri, projection, null, null, null)
+    val cursor = contentResolver.query(allImagesUri, projection, null, null, null)
     cursor?.let {
         try {
             while (cursor.moveToNext()) {
 
-                val name =
-                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
-                val folder =
-                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME))
-                val datapath =
-                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
-                var folderpaths = datapath.substring(0, datapath.lastIndexOf("$folder/"))
+                val dataPath = cursor.getStringOrEmpty(projection[0])
+                val name = cursor.getStringOrEmpty(projection[1])
+                val folder = cursor.getStringOrEmpty(projection[2])
+
+                var folderpaths = dataPath.substring(0, dataPath.lastIndexOf("$folder/"))
 
                 folderpaths = "$folderpaths$folder/"
-                Log.w("TESTEs", folderpaths)
 
                 if (!picPaths.contains(folderpaths)) {
 
@@ -81,7 +72,7 @@ fun Context.getAllImageFolders(): ArrayList<ImageFolderData> {
                         name,
                         folderpaths,
                         folder,
-                        datapath
+                        dataPath
                     )
 
                     folderData.addpics()
@@ -91,7 +82,7 @@ fun Context.getAllImageFolders(): ArrayList<ImageFolderData> {
                 } else {
                     for (i in picFolderData.indices) {
                         if (picFolderData[i].path == folderpaths) {
-                            picFolderData[i].firstPic = datapath
+                            picFolderData[i].firstPic = dataPath
                             picFolderData[i].addpics()
                         }
                     }
@@ -103,11 +94,20 @@ fun Context.getAllImageFolders(): ArrayList<ImageFolderData> {
     }
 
     cursor?.close()
-    for (i in picFolderData.indices) {
-        Log.w(
-            "picture folders",
-            picFolderData[i].folderName + " and path = " + picFolderData[i].path + " " + picFolderData[i].numberOfPics
-        )
-    }
+//    for (i in picFolderData.indices) {
+//        Log.w(
+//            "picture folders",
+//            picFolderData[i].folderName + " and path = " + picFolderData[i].path + " " + picFolderData[i].numberOfPics
+//        )
+//    }
     return picFolderData
+}
+
+private fun getDatabaseKeys(): Array<String> {
+    return arrayOf(
+        MediaStore.Images.Media.DATA,
+        MediaStore.Images.Media.DISPLAY_NAME,
+        MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+        MediaStore.Images.Media.BUCKET_ID
+    )
 }
